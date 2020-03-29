99ae3bcfc31e7106d661844892e4aa7f1b694215
chivagarg <chivagarg@gmail.com>
2020-03-29 15:58:38 -0700

    return 403 if user doesnt pass filter

---
+++ src/main/java/com/c1b1/v2/studyone/studyone/auth/RestExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.c1b1.v2.studyone.studyone.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+
+}
--- src/main/java/com/c1b1/v2/studyone/studyone/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/c1b1/v2/studyone/studyone/security/jwt/JwtTokenFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtTokenFilter extends GenericFilterBean {
@@ -24,9 +25,19 @@ public class JwtTokenFilter extends GenericFilterBean {
         // Fetch token from the inbound request
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
 
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = (token != null) ? jwtTokenProvider.getAuthentication(token) : null;
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = (token != null) ? jwtTokenProvider.getAuthentication(token) : null;
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        // Note that the ExceptionHandler will only work if the request is handled by the DispatcherServlet
+        // However this exception occurs before that as it is thrown by a Filter. So we will never be able to
+        // handle this exception with an (@)ExceptionHandler
+        catch (InvalidJwtAuthenticationException e) {
+            HttpServletResponse response=(HttpServletResponse) res;
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
 
         filterChain.doFilter(req, res);
