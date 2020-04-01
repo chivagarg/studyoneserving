7040d93b21d1908e2161d8c3ecb8b826a3aeca73
chivagarg <chivagarg@gmail.com>
2020-03-31 22:23:06 -0700

    added query in user repo

--- src/main/java/com/c1b1/v2/studyone/studyone/repository/UserRepository.java
+++ src/main/java/com/c1b1/v2/studyone/studyone/repository/UserRepository.java
@@ -2,9 +2,15 @@ package com.c1b1.v2.studyone.studyone.repository;
 
 import com.c1b1.v2.studyone.studyone.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    // The following is much slower
+    @Query("select u from User u where u.username = ?1")
+    Optional<User> queryByUsername(String emailAddress);
 }
